5008aa989e68ce2bfae8f544f0fd4549af1a9df5
김수빈 <77378559+subinkim26@users.noreply.github.com>
2023-05-09 20:53:58 +0900

    feat : bankingtest share button update
     금융테스트 sns 공유 버튼 => 결과 내용 + 어플 플레이스토어 링크

--- app/src/main/java/com/example/personality_style_test/bankingtest/BankingTestResult1Fg.java
+++ app/src/main/java/com/example/personality_style_test/bankingtest/BankingTestResult1Fg.java
@@ -1,7 +1,12 @@
 package com.example.personality_style_test.bankingtest;
 
+
+
+
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.personality_style_test.PersonalityActivity;
 import com.example.personality_style_test.R;
@@ -43,6 +49,7 @@ public class BankingTestResult1Fg extends Fragment {
 
         Button bankingTestRestart = (Button) BankingTestResult1.findViewById(R.id.bankingtest_restart);
         Button bankingTestList = (Button) BankingTestResult1.findViewById(R.id.bankingtest_list);
+        ImageButton bankingTestShare = (ImageButton) BankingTestResult1.findViewById(R.id.share_button);
 
         //현재 액티비를 가져온다
         Activity thisActivity = getActivity();
@@ -70,6 +77,20 @@ public class BankingTestResult1Fg extends Fragment {
                     //지금껀 종료
                     thisActivity.finish();
                 }
+            }
+        });
+
+        bankingTestShare.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                //결과 내용 + 어플링크
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, "금융테스트 결과 - 완벽한 재테크 고목인 당신! \n" + "https://play.google.com/store/apps/details?id=com.mk.personality_style_test&hl=ko");
+                startActivity(Intent.createChooser(sharingIntent, "결과를 공유할 앱을 선택해주세요"));
+
+
             }
         });
 
